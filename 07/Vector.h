5c98507f0a242b4abbca4b703738133804a8276e
danroor <60691905+danroor@users.noreply.github.com>
2020-04-21 16:29:55 +0300

    Add files via upload

---
+++ 07/Vector.h
@@ -0,0 +1,242 @@
+#pragma once
+#include <exception>
+#include <limits>
+#include <iterator>
+
+template <class T>
+class Allocator
+{
+    using ptr_t = T*;
+    using constref_t = const T&;
+public:
+    ptr_t alloc(size_t count) {
+        ptr_t p = (ptr_t) calloc(count, sizeof(T));
+        if (p == nullptr) {
+            throw std::bad_alloc();
+        }
+        return p;
+    }
+
+    void dealloc(ptr_t p, size_t count) {
+        free(p);
+    }
+
+    size_t max_size() const noexcept {
+        return std::numeric_limits<size_t>::max() / sizeof(T);
+    }
+
+    void construct(ptr_t p, constref_t val, size_t n) {
+        for (size_t i = 0; i < n; i++) {
+            new (p + i) T(val);
+        }
+    }
+
+    void construct(ptr_t p, size_t n) {
+        for (size_t i = 0; i < n; i++) {
+            new (p + i) T();
+        }
+    }
+
+    void destroy(ptr_t p, size_t n) {
+        for (size_t i = 0; i < n; i++) {
+            (p[i]).~T();
+        }
+    }
+};
+
+template <class T>
+class Iterator {
+    using ptr_t = T*;
+    using valconstref_t = T&;
+    using constref_t = const Iterator<T>&;
+    
+    ptr_t ptr_;
+    bool reverse_;
+public:
+    explicit Iterator(ptr_t ptr, bool reverse) : ptr_(ptr), reverse_(reverse) {}
+
+    bool operator==(constref_t other) const {
+        return ptr_ == other.ptr_;
+    }
+
+    bool operator!=(constref_t other) const {
+        return ptr_ != other.ptr_;
+    }
+
+    valconstref_t operator*() const {
+        return *ptr_;
+    }
+
+    Iterator& operator++() {
+        if (reverse_) {
+            --ptr_;
+        } else {
+            ++ptr_;
+        }
+        return *this;
+    }
+
+    Iterator operator++(int) {
+        Iterator it(ptr_, reverse_);
+        if (reverse_) {
+            --ptr_;
+        } else {
+            ++ptr_;
+        }
+        return it;
+    }
+
+    Iterator& operator--() {
+        if (reverse_) {
+            ++ptr_;
+        } else {
+            --ptr_;
+        }
+        return *this;
+    }
+
+    Iterator operator--(int) {
+        Iterator it(ptr_, reverse_);
+        if (reverse_) {
+            ++ptr_;
+        } else {
+            --ptr_;
+        }
+        return it;
+    }
+
+    Iterator& operator+(size_t n) {
+        if (reverse_) {
+            ptr_ -= n;
+        } else {
+            ptr_ += n;
+        }
+        return *this;
+    }
+
+    Iterator& operator-(size_t n) {
+        if (reverse_) {
+            ptr_ += n;
+        } else {
+            ptr_ -= n;
+        }
+        return *this;
+    }
+};
+
+template <class T, class AllocT = Allocator<T> >
+class Vector
+{
+    using ptr_t = T*;
+    using ref_t = T&;
+    using constref_t = const T&;
+    using iterator = Iterator<T>;
+
+    size_t sz, cap; //size and capacity
+    AllocT alloc_;
+    ptr_t data;
+public:
+
+    explicit Vector() : sz(0), cap(0), alloc_(AllocT()), data(nullptr) {}
+
+    explicit Vector(size_t count) : sz(count), cap(count), alloc_(AllocT()) {
+        data = alloc_.alloc(count);
+        alloc_.construct(data, count);
+    }
+
+    Vector(size_t count, constref_t val) : sz(count), cap(count), alloc_(AllocT())  {
+        data = alloc_.alloc(count);
+        alloc_.construct(data, val, count);
+    }
+
+    ~Vector() {
+        alloc_.destroy(data, sz);
+        alloc_.dealloc(data, cap);
+    }
+
+    iterator begin() noexcept {
+        return iterator(data, false);
+    }
+    iterator end() noexcept {
+        return iterator(data + sz, false);
+    }
+
+    iterator rbegin() noexcept {
+        return iterator(data + sz - 1, true);
+    }
+    
+    iterator rend() noexcept {
+        return iterator(data - 1, false);
+    }
+
+    void push_back(T&& value) {
+        this->resize(sz + 1);
+        this->data[sz - 1] = std::move(value);
+    }
+    void push_back(const T& value) {
+        this->resize(sz + 1);
+        this->data[sz - 1] = value;
+    }
+
+    void pop_back() {
+        if (sz > 0)
+            alloc_.destroy(data + --sz, 1);
+    }
+
+    void reserve(size_t count) {
+        if (count > alloc_.max_size())
+            throw std::length_error("Too much elements to reserve");
+        if (count <= cap)
+            return;
+
+        ptr_t newdata = alloc_.alloc(count);
+        alloc_.construct(newdata, count);
+        std::copy(data, data + sz, newdata);
+        alloc_.destroy(data, sz);
+        alloc_.dealloc(data, cap);
+        data = newdata;
+        cap = count;
+    }
+
+    void resize(size_t newsz) {
+        if (newsz == sz) return;
+        if (newsz < sz) {
+            alloc_.destroy(data + newsz, sz - newsz);
+            sz = newsz;
+            return;
+        }
+
+        if (newsz > cap)
+            this->reserve(newsz > 2 * cap ? newsz : 2 * cap);
+
+        alloc_.construct(data + sz, newsz - sz);
+        sz = newsz;
+    }
+
+    void resize(size_t newsz, constref_t val) {
+        if (newsz == sz) return;
+        if (newsz < sz) {
+            alloc_.destroy(data + newsz, sz - newsz);
+            sz = newsz;
+            return;
+        }
+
+        if (newsz > cap)
+            this->reserve(newsz > 2 * cap ? newsz : 2 * cap);
+
+        alloc_.construct(data + sz, val, newsz - sz);
+        sz = newsz;
+    }
+
+    void clear() noexcept {
+        alloc_.destroy(data, sz);
+        sz = 0;
+    }
+
+    ref_t operator[](size_t i) { return data[i]; }
+    constref_t operator[](size_t i) const { return data[i]; }
+
+    bool empty() const noexcept { return sz == 0; }
+    size_t size() const noexcept { return sz; }
+    size_t capacity() const noexcept { return cap; }
+};
---
+++ 07/test.cpp
@@ -0,0 +1,83 @@
+#include <cassert>
+#include <iostream>
+
+#include "Vector.h"
+
+using namespace std;
+
+
+int main()
+{
+    Vector<int> vi1;
+    Vector<int> vi2(5);
+    Vector<string> vs(10, "shazam");
+
+    assert(vi1.size() == 0 && vi1.capacity() == 0);
+    assert(vi2.size() == 5 && vi2.capacity() == 5);
+    assert(vs.size() == 10 && vs.capacity() == 10);
+
+    assert(vi1.empty());
+    assert(!vi2.empty());
+
+    vi1.pop_back();
+    vi2.pop_back();
+    assert(vi2.size() == 4);
+
+    vs[0] = "badum";
+    cout << vs[0] << endl << vs[1] << endl;
+    
+    vs.reserve(11);
+    vs.push_back("123");
+    vs.push_back("456");
+    assert(vs.size() == 12);
+
+    int x = 1;
+    vi1.push_back(x);
+    vi1.push_back(move(x));
+    assert(vi1.size() == 2);
+
+    vi2.resize(2);
+    for (int i = 0; i < 2; ++i)
+        cout << vi2[i] << " ";
+    cout << endl;
+
+    vi2.resize(4);
+    for (int i = 0; i < 4; ++i)
+        cout << vi2[i] << " ";
+    cout << endl;
+
+    vi2.resize(7, 7);
+    for (int i = 0; i < 7; ++i)
+        cout << vi2[i] << " ";
+    cout << endl;
+    assert(vi2.capacity() == 10); // capacity == 2 
+                                  //7 < 5 * 2 -> capacity *= 2
+
+    vs.reserve(30);
+    assert(vs.capacity() >= 30);
+
+    for (auto it = vi2.begin(); it != vi2.end(); ++it) {
+        cout << *it << " ";
+    }
+    cout << endl;
+
+    assert(*(vi2.begin() + 2) == 0);
+    assert(*(vi2.end() - 2) == 7);
+    auto it1 = vi2.end();
+    --it1;
+    it1--;
+    assert(*it1 == 7);
+
+    for (auto it = vi2.rbegin(); it != vi2.rend(); it++) {
+        cout << *it << " ";
+    }
+    cout << endl;
+
+    assert(*(vi2.rbegin() + 2) == 7);
+    assert(*(vi2.rend() - 2) == 0);
+    
+    vs.clear();
+    assert(vs.size() == 0 && vs.begin() == vs.end());
+
+    return 0;
+}
